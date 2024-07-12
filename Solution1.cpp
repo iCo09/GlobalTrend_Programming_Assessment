5e082f66bb24a719a05d1aa4c75898beee56ecb0
Rahul Bisht <97499344+iCo09@users.noreply.github.com>
2024-07-12 16:31:42 +0000

    Sol1

--- Solution1.cpp
+++ Solution1.cpp
@@ -1,1 +1,17 @@
-
+//Reverse a Linked List
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL)return NULL;
+        ListNode *p = NULL;
+        ListNode *c = head;
+        ListNode *n = head->next;
+        while(c != NULL){
+            c -> next = p;
+            p = c;
+            c = n;
+            if(n != NULL)n = n -> next;
+        }
+        return p;
+    }
+};
