759be2cc5122c9e3862796d5341440254eb74a6f
Rahul Bisht <97499344+iCo09@users.noreply.github.com>
2024-07-12 17:01:23 +0000

    Sol5

---
+++ Solution5.cpp
@@ -0,0 +1,11 @@
+//Rotate an array by k places
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+            vector<int> ans(nums.size());
+            for(int i = 0; i<nums.size(); i++){
+              ans[(i+k) % nums.size()] = nums[i];      
+            }
+            nums = ans;
+    }
+};
